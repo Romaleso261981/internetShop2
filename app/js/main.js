adb07b88603da379a07474c3bbba55f136ae665d
Roman lesyo <ladiginscormag@gmail.com>
2023-05-14 13:58:36 +0300

    added function closed rightsite-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function () {
+    $(".header__btn").on("click", function () {
+      $(".rightsite-menu").removeClass("rightsite-menu--close");
+    });
 
-})
+    $(".rightsite-menu__close").on("click", function () {
+    $(".rightsite-menu").addClass("rightsite-menu--close");
+  });
+});
